6efdd8dad697d6491b55f2361f764cf70574dc6d
imnotgittinthis <34953965+imnotgittinthis@users.noreply.github.com>
2017-12-30 11:31:28 -0700

    Update binomial.py

--- binomial.py
+++ binomial.py
@@ -1,5 +1,9 @@
 # runs as is for python 2.7 interpreter.
-
+# prints out the binomial coefficients for (x+y)^n, where n is a nonnegative integer.
+# Example (Ubuntu terminal):
+# python2 binomial.py
+# Enter exponent: 11
+# [1, 11, 55, 165, 330, 462, 462, 330, 165, 55, 11, 1]
 def factorial(n):
     if n == 0:
         return 1
@@ -15,4 +19,3 @@ nfac = factorial(n)
 for j in range(0,n+1):
     coeffs.append(nfac/(factorial(j)*factorial(n-j)))
 print coeffs
-
