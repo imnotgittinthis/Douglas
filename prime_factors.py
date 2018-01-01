4fc987d4f65e1afc495d942ad5d8ec5a1d782a63
imnotgittinthis <34953965+imnotgittinthis@users.noreply.github.com>
2018-01-01 12:06:36 -0700

    Update prime_factors.py

--- prime_factors.py
+++ prime_factors.py
@@ -1,36 +1,26 @@
 # Runs as is for python 2.7 interpreter.
-def prime_list(n):
-    """
-    prime_factors(n) prints the prime factorization of a positive integer >= 2.
-    The code is fast only for integers 5 digits or fewer.
-    
-    Example (Ubuntu terminal):
-    $ python2 prime_factors.py
-    Enter integer: 89556
-    [2, 2, 3, 17, 439]
-    """
-    if n <= 1:
-        pass
-    else:
-        primes = range(2,n/2+1)
-        i = 0
-        j = 2
-        while primes[i]*j in primes:
-            while primes[i]*j in primes:
-                primes.remove(primes[i]*j)
-                j += 1
-            else:
-            	i += 1
-        	j = primes[i]
-        	continue
-        factors = []
-        while n>1:
-            for p in primes:
-            	while n%p == 0:
-                    n /= p
-                    factors.append(p)
-        return factors
-
+# prime_factors prints the prime factorization of a positive integer >= 2.
+# The code is fast only for integers 5 digits or fewer.
+# Example (Ubuntu terminal):
+# python2 prime_factors.py
+# Enter integer: 56225
+# [5, 5, 13, 173]
 n = int(raw_input("Enter integer: "))
-factors = prime_list(n)
+import math 
+count = 1
+primes = [2]
+for i in range(2,n+1):
+    val = True
+    for j in range(2,int(math.floor(math.sqrt(i)))+1):
+        if i % j == 0:
+            val = False
+    if val:
+        primes.append(i)
+factors = []
+while n>1:
+    for p in primes:
+    	while n%p == 0:
+            n /= p
+            factors.append(p)
+
 print factors
